85ba73a37d8852c13f769934ee5eaa676cf6a927
RangerAcodes <mailaditkumar@gmail.com>
2024-02-28 15:29:04 +0530

    adding @ElasticsearchRepository annotation

--- main/java/com/example/productservice_proxy/ProductServiceProxyApplication.java
+++ main/java/com/example/productservice_proxy/ProductServiceProxyApplication.java
@@ -6,9 +6,11 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 
+@EnableElasticsearchRepositories //(basePackages = "com.example.productservice_proxy.ElasticsearchRepo")
 @SpringBootApplication //(scanBasePackages = {"com.example.productservice_proxy", "com.example.productservice_proxy.Repositories"})
 public class ProductServiceProxyApplication {
 
